0f1f05bfb8d4c77ae04f96fc301d8ca4a293b5bd
“pratushrai0309” <pratushrai0309@gmail.com>
2022-08-06 12:56:12 +0530

    Error

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 #![allow(dead_code, unused)]
 mod token;
-use crate::token::tokenize;
+use crate::token::{tokenize, Token};
 
 fn main(){
-   let tokens = tokenize("x - z + 9 * 8;").unwrap();
+   let tokens = tokenize("6 + 9;").unwrap();
    for token in tokens{
        println!("{:?}", token)
    }
-   println!("Hello World!");
-   println!("Tokenizer")
+   println!("Tokenizer");
+   
 }
 
