7215e0caec6cf619c685a835c49f03f70f8cd170
“pratushrai0309” <pratushrai0309@gmail.com>
2022-08-09 02:16:04 +0530

    Parser -- Variable Assigment

--- src/parser.rs
+++ src/parser.rs
@@ -22,13 +22,13 @@ pub struct Ast<T>{
 pub fn parse(tokens: Vec<Token>) -> Vec<Ast<Assigment>>{
     let mut asts = Vec::new();
     let mut current_node: Option<Ast<Assigment>> = None;
-    for (i , token) in tokens.iter().enumerate(){
+    for (i, token) in tokens.iter().enumerate(){
         if token.ttype == TokenType::Assign{
             current_node = Some(Ast{
                 ty: AstType::VariableAssign,
                 value: Assigment{
-                    variable: token.value.to_string(),
-                    value: token.value.to_string()
+                    variable: tokens[i - 1].value.to_string(),
+                    value: tokens[i + 1].value.to_string()
                 }
             });
             asts.push(current_node.unwrap())
