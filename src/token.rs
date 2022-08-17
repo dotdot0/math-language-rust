e57887c837c3edb3d5dcddab1b64292e3b217779
“pratushrai0309” <pratushrai0309@gmail.com>
2022-08-18 02:01:21 +0530

    Token -> Option

--- src/token.rs
+++ src/token.rs
@@ -41,79 +41,77 @@ impl Token{
 }
 
 pub fn tokenize(code: &String) -> Result<Vec<Token>, Error>{
-    let mut current_token: Token = Token{
-        ttype: TokenType::Start,
-        value: String::from("codestart")
-    };
+    let mut current_token: Option<Token> = None;
     let mut tokens = Vec::new();
     for ch in code.chars(){
         if ch == '+'{
-            current_token = Token{
+            current_token = Some(Token{
                 ttype: TokenType::Add,
                 value: String::from("+")
-            };  
-            tokens.push(current_token);
+            });  
+            tokens.push(current_token.unwrap());
         }
         else if ch == '-' {
-            current_token = Token{
+            current_token = Some(Token{
                 ttype: TokenType::Subtract,
                 value: String::from("-") 
-            };
-            tokens.push(current_token)
+            });
+            tokens.push(current_token.unwrap())
         }
         else if ch == '*' {
-            current_token = Token{
+            current_token = Some(Token{
                 ttype: TokenType::Multiply,
                 value: String::from("*")
-            };
-            tokens.push(current_token)
+            });
+            tokens.push(current_token.unwrap())
         }
         else if ch == '/' {
-           current_token = Token{
+           current_token = Some(Token{
                ttype : TokenType::Divide,
                value : String::from("/")
-        };
-           tokens.push(current_token)
+        });
+           tokens.push(current_token.unwrap())
         }
         else if ch.is_whitespace(){
-           current_token = Token{
+           current_token = Some(Token{
                   ttype: TokenType::WhiteSpace,
                   value: String::from("WhiteSpace")
-           };
+           });
         }
         else if ch == ';'{
-            current_token = Token{
+            current_token = Some(Token{
                 ttype: TokenType::End,
                 value: String::from(";")
-            };
-            tokens.push(current_token)
+            });
+            tokens.push(current_token.unwrap())
         }
         else if ch.is_numeric(){
-            current_token = Token{
+            current_token = Some(Token{
                 ttype : TokenType::Number,
                 value : String::from(ch)
-            };
-            tokens.push(current_token)
+            });
+            tokens.push(current_token.unwrap())
         }
         else if ch == ')'{
-            current_token = Token{
+            current_token = Some(Token{
                 ttype : TokenType::Rparen,
                 value : String::from(")")
-            }
+            });
+            tokens.push(current_token.unwrap())
         }
         else if ch == '('{
-            current_token = Token{
+            current_token = Some(Token{
                 ttype : TokenType::Lparen,
                 value : String::from("(")
-            };
-            tokens.push(current_token)
+            });
+            tokens.push(current_token.unwrap())
         }
         else if ch == '='{
-            current_token = Token{
+            current_token = Some(Token{
                 ttype : TokenType::Assign,
                 value : String::from("=")
-            };
-            tokens.push(current_token)
+            });
+            tokens.push(current_token.unwrap())
         }
         else {
             panic!("Invalid Token")
