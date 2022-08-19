1cdd50ad3acbc1d7a2e12cbc4bad4f60fe9cd5b6
“pratushrai0309” <pratushrai0309@gmail.com>
2022-08-20 00:23:33 +0530

    REPL

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 mod token;
 mod parser;
 use std::io::{self, Write};
+use std::usize;
 use crate::token::{tokenize, Token, TokenType};
 use crate::parser::parse; 
 
 fn main(){
    loop{
      let mut code: String = String::new();
+     let len: usize = code.len();
      print!(">");
      let _ = io::stdout().flush();
      io::stdin().read_line(&mut code).expect("Unable To Read Source Code");
